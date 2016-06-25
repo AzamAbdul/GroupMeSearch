6f6739911c67c7cecd3ece14b3a11570f0d312d6
azmabdul <abdulkadirazam@gmail.com>
2016-06-25 04:00:48 -0400

    removed storing of messages into txt file and updated it to now store messages in a mongodb collection

--- miner.rb
+++ miner.rb
@@ -1,6 +1,6 @@
 require 'net/http'
 require 'json'
-
+require 'mongo'
 def page_through(group_id, tok,before_id=nil)
 	url = "https://api.groupme.com/v3/groups/#{group_id}/messages"
 	uri = URI(url)
@@ -17,6 +17,29 @@ def page_through(group_id, tok,before_id=nil)
 	res = Net::HTTP.get_response(uri)
 	return res
 end
+def insert_into_db(connection,message_json)	
+
+end
+
+def estab_db_connection()
+	config_table= YAML::load_file('search.conf')
+	host = config_table["mongodb"]["host"]
+	port = config_table["mongodb"]["port"]
+	connection_string= host.to_s+":"+port.to_s
+
+	return Mongo::Client.new([ connection_string], :database => 'GroupMeSearch')
+end
+
+
+def collection_exists?(client,name)
+	client.collections.each{
+		|collection|
+			if(collection.name==name)
+				return true
+			end
+	}
+	return false
+end
 
 def mine(tok, group_id)
 	res = page_through(group_id, tok)
@@ -28,9 +51,21 @@ def mine(tok, group_id)
 
 		res = page_through(  group_id,tok,messages[messages.length-1]["id"])
 	end
-	filename = group_id+'.txt'
-	if(!File.file?(filename))
-		f = File.open(filename, "w")
-		messages.each{|m| f.puts JSON.generate(m)}
+	
+	mongo_client = estab_db_connection()
+	# filename = group_id+'.txt'
+	
+	if(not(collection_exists?(mongo_client, group_id)))
+		# f = File.open(filename, "w")
+		mongo_client[group_id.to_s].create()
+		messages.each{|m|
+			m_json = JSON.generate(m) 
+			# f.puts m_json
+		puts " "
+			puts m_json
+		puts " "
+			mongo_client[group_id.to_s].insert_one(m)
+		}
+
 	end
 end
